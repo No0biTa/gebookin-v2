2b27dc3e6939180104f42d9708276dee6aac1070
Akbar <firmanasya1601@gmail.com>
2024-07-01 17:53:49 +0700

    coba edua

--- admin/src/pages/new/New.jsx
+++ admin/src/pages/new/New.jsx
@@ -33,7 +33,7 @@ const New = ({ inputs, title }) => {
         img: url,
       };
 
-      await axios.post("https://gebookin-admin.vercel.app/auth/register", newUser);
+      await axios.post("/auth/register", newUser);
       navigate("/users");
     } catch (err) {
       console.log(err);
